cee23d97d929c3662b6645980a09967b9c3974d4
Andrew Boltachev <andrewboltachev@gmail.com>
2023-02-09 19:03:47 +0300

    fix top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -140,7 +140,7 @@ const GameFinish = (props) => {
 };
 
 const PageNotFound = () => {
-  return <div style={{position: "fixed", top: 0, left: 0, right: 0, bottom: 0, display: "flex", justifyContent: "center", alignItems: "center", zIndex: -1}}>
+  return <div style={{position: "fixed", top: 60, left: 0, right: 0, bottom: 0, display: "flex", justifyContent: "center", alignItems: "center"}}>
     <div><Trans>Page not found</Trans>. <Link to="/"><Trans>Visit Home</Trans></Link></div>
   </div>;
 }
