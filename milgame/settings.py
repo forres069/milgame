d3183833beed2e54fd4b4bc9c03fb73825b0912b
Andrew Boltachev <andrewboltachev@gmail.com>
2023-02-09 12:58:37 +0300

    LANGUAGES

--- milgame/settings.py
+++ milgame/settings.py
@@ -110,7 +110,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = [
+  ('en', 'English'),
+  ('ru', 'Русский'),
+  ('fr', 'Français'),
+]
 
 TIME_ZONE = 'UTC'
 
