b2dbf32a1d074307378094bdce439607d3674233
Andrew Boltachev <andrewboltachev@gmail.com>
2023-02-09 08:17:15 +0300

    STATIC_ROOT

--- milgame/settings.py
+++ milgame/settings.py
@@ -124,5 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 FRONTEND_DEV_MODE = os.environ.get('FRONTEND_DEV_MODE', False)
